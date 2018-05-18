f11a272f3b5d6ffd2ad9fe1d37135d6c2277c8d5
hnyilee <39369328+hnyilee@users.noreply.github.com>
2018-05-18 14:03:32 -0300

    Update rule.ctrl.php
    translate

--- app/controller/home/rule.ctrl.php
+++ app/controller/home/rule.ctrl.php
@@ -1,4 +1,4 @@
 <?php
 defined('IN_IA') or exit('Access Denied');
-$pagetitle = !empty($config['tginfo']['sname']) ? '拼团规则 - '.$config['tginfo']['sname'] : '拼团规则';
+$pagetitle = !empty($config['tginfo']['sname']) ? 'regra de compra do grupo - '.$config['tginfo']['sname'] : 'regra de compra do grupo';
 include wl_template('home/rule');
